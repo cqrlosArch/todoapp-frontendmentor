85c1898ed7cc75dcb3fc7a100a1a29dfa1470c58
cqrlosArch <diaz.flores.c@gmail.com>
2020-12-01 18:59:56 +0100

    button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,12 +5,12 @@ const ButtonStyled = styled.button`
   border: none;
   display: block;
   position: absolute;
-
   z-index: 10;
   height: 20px;
   width: 20px;
   border-radius: 50%;
-  border: 1px solid ${({ theme }) => theme.buttonInput};
+  border: 1px solid
+    ${({ theme, status }) => (status ? 'none' : theme.buttonInput)};
   top: 13px;
   left: ${({ left }) => left};
   background: ${({ status, theme }) =>
